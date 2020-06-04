d9af888818a5f82c948f43211a0e42b25c52d7b0
madaniloff <madstagherd@gmail.com>
2020-06-03 23:20:09 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -29,11 +29,14 @@ struct Student {
 
 //Initialize functions
 void addStudent(Student **arr, Student* newStudent, int size);
+bool checkCollision(Student **arr, Student* newStudent, int size);
 void generateStudent(int num);
 void printStudent(Student **arr, int size);
+void delStudent(Student **arr, int inid, int size);
 
 int main() {
   int size = 100;
+  int inid;
   srand(time(NULL));
   Student** arr = new Student*[100];
   bool running = true;
@@ -66,6 +69,25 @@ int main() {
 	cout << "GPA: " << endl;
 	cin >> newStudent->gpa;
 	cin.ignore();
+	//If there will be a collision with a chain of 3
+	if (checkCollision(arr, newStudent, size) == true) {
+	  //Double the size of hash table
+	  size = size * 2;
+	  //Create temp array
+	  Student* temp = newStudent();
+	  for (int i = 0; i < size/2; i++ ) {
+	    temp[i] = list[i];
+	  }
+	  //Resize arr and clear it
+	  temp = new Student*[size];
+	   for (int i = 0; i < size; i++ ) {
+	    list[i] = NULL;
+	  }
+	   //Readd students to new list
+	   
+	}
+	
+      
 	//Add all fields to hash function
 	addStudent(arr, newStudent, size);
       }
@@ -85,7 +107,10 @@ int main() {
     }
     //DELETE
     else if (strcmp(input, "DELETE") == 0) {
-      
+      cout << "Enter ID to delete" << endl;
+      cin >> inid;
+      cin.ignore();
+      delStudent(arr, inid, size);
     }
     //QUIT
     else if (strcmp(input, "QUIT") == 0) {
@@ -109,12 +134,24 @@ void addStudent(Student **arr, Student* newStudent, int size) {
       arr[code]->next = newStudent;
       (arr[code]->next)->prev = arr[code]->next;
     }
-    else {
-      while (arr[code]->next != NULL) {
-	arr[code] = arr[code]->next;
-	(arr[code]->next)->prev = arr[code]->next;
-      }
+    //If next's next is NULL
+    else if (arr[code]->next->next == NULL) {
+      arr[code]->next->next = newStudent;
+      (arr[code)->next->next)->prev = arr[code]->next->next;
     }
+  }
+}
+
+//Check for collisions
+bool checkCollision(Student **arr, Student* newStudent, int size) {
+  //Get code
+  int code = newStudent-> id % size;
+  //If that index already has three chains
+  if (arr[code]->next->next != NULL) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
@@ -134,6 +171,7 @@ void generateStudent(int num) {
 
 //Print out the students
 void printStudent(Student **arr, int size) {
+  cout << "-------------------------" << endl;
   for (int i = 0; i < size; i++) {
     if (arr[i] != NULL) {
       cout << "First name: " << arr[i]->fname << endl;
@@ -143,16 +181,63 @@ void printStudent(Student **arr, int size) {
       cout << "-------------------------" << endl;
       //If there is a chain
       if (arr[i]->next != NULL) {
-	while (arr[i]->next != NULL) {
+	cout << "First name: " << arr[i]->next->fname << endl;
+	cout << "Last name: " << arr[i]->next->lname << endl;
+	cout << "ID: " << arr[i]->next->id << endl;
+	cout << "GPA: " << arr[i]->next->gpa << endl;
+	cout << "-------------------------" << endl;
+	if (arr[i]->next->next != NULL) {
 	  cout << "First name: " << arr[i]->next->fname << endl;
 	  cout << "Last name: " << arr[i]->next->lname << endl;
 	  cout << "ID: " << arr[i]->next->id << endl;
 	  cout << "GPA: " << arr[i]->next->gpa << endl;
 	  cout << "-------------------------" << endl;
-	  arr[i] = arr[i]->next;
-	  (arr[i]->next)->prev = arr[i]->next;
 	}
       }
     }
-  }				  
+  }
+}				  
+
+
+//Delete a student
+void delStudent(Student **arr, int inid, int size) {
+  //Find index that the id is at
+  int index = inid % size;
+  //If student doesn't exist
+  if (arr[index] == NULL) {
+    cout << "This student does not exist!" << endl;
+  }
+  //If student exists and is the first node
+  else if (arr[index] != NULL && arr[index]->id == inid) {
+    //If there is no chaining
+    if (arr[index]->next == NULL) {
+      arr[index] = NULL;
+    }
+    //If there is
+    else {
+      //Replace the first one with the next one
+      Student* temp = arr[index]->next;
+      temp->prev = NULL;
+      arr[index] = temp;
+      
+    }
+  }
+  //If there is chaining
+  else {
+    //If there is no next
+    if (arr[index]->next == NULL) {
+      cout << "This student does not exist! " << endl;
+    }
+    else {
+      //If the second chain's id matches
+      if (arr[index]->next->id == inid) {
+	//If it's next is NULL
+	if (arr[index]->next->next == NULL) {
+	  
+	}
+      }
+    }
+  }
+  
+
 }
